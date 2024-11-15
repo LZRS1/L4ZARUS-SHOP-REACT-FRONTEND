d0fda39fa275dd84af2dce2708d4d1562a060183
LZRS1 <153413019+LZRS1@users.noreply.github.com>
2024-11-14 21:39:48 -0600

    Update ShopContext.jsx

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -7,7 +7,7 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
-    const currency = 'MXN';
+    const currency = '$';
     const delivery_fee = 1;
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search, setSearch] = useState('');
