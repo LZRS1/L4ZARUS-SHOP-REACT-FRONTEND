2ee0e92f3298f35d551fd244447a52099aa43b89
LZRS1 <153413019+LZRS1@users.noreply.github.com>
2024-11-18 08:33:13 -0600

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Nuestra tienda</p>
-          <p className=' text-gray-500'><a href="https://l4zarus.site/">l4zarus.site</a></p>
+          <p className=' text-gray-500'><a href="https://l4zarus.store/">l4zarus.store</a></p>
           <p className=' text-gray-500'>Tel: (+52) 55-2069-2985<br /> Email: l4zarus@l4zarus.com</p>
           <p className='font-semibold text-xl text-gray-600'>Ofertas de trabajo.</p>
           <p className=' text-gray-500'>Obtenga más información sobre nuestros equipos y ofertas de trabajo.</p>
