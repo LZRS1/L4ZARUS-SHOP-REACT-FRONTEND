e508ab541a55ebe67dae6a8ea3710d6df2aa95c1
LZRS1 <153413019+LZRS1@users.noreply.github.com>
2024-12-10 17:27:47 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
@@ -9,11 +10,13 @@ const Hero = () => {
             <div className='text-[#414141]'>
                 <div className='flex items-center gap-2'>
                     <p className='w-8 md:w-11 h-[2px] animated-gradients'></p>
-                    <p className=' font-medium text-sm md:text-base gradient-text3'>NUESTRO PRODUCTO</p>
+                    <p className=' font-medium text-sm md:text-base gradient-text3'>L4ZARUS CORP</p>
                 </div>
-                <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed gradient-text'>MAS VENDIDO</h1>
+                <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed gradient-text'>La mejor tienda de servicios digitales</h1>
                 <div className='flex items-center gap-2'>
-                    <p className='font-semibold text-sm md:text-base gradient-text3'>COMPRA AHORA</p>
+                <p className='font-semibold text-sm md:text-base gradient-text3'>
+    <Link to="/collection">COMPRA AHORA</Link>
+</p>
                     <p className='w-8 md:w-11 h-[2px] animated-gradients'></p>
                 </div>
             </div>
