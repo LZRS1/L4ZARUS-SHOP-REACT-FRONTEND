d2b7cdcc39587ae2fa857d92cf0e2cd5b2347c3e
LZRS1 <153413019+LZRS1@users.noreply.github.com>
2024-12-10 17:51:49 -0600

    Update Collection.jsx

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -94,6 +94,9 @@ const Collection = () => {
         <div className={`gradient-border pl-5 py-3 mt-6 ${showFilter ? '' :'hidden'} sm:block`}>
           <p className='mb-3 text-sm font-medium gradient-text'>CATEGORIAS (A-Z)</p>
           <div className='flex flex-col gap-2 text-sm font-light gradient-text3'>
+          <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'PAQUETES'} onChange={toggleCategory}/>  PAQUETES
+            </p>
             <p className='flex gap-2'>
               <input className='w-3' type="checkbox" value={'SPOTIFY'} onChange={toggleCategory}/>  SPOTIFY
             </p>
